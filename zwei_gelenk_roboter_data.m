c984288bdc19af30ba660ee6bf72b69330c13089
Mattis Ritter <102912075+mattisritter@users.noreply.github.com>
2022-10-09 11:19:07 +0200

    Create zwei_gelenk_roboter_data.m

---
+++ zwei_gelenk_roboter_data.m
@@ -0,0 +1,6 @@
+%% Robot
+P_m1 = 10; % elbow motor mass [ kg ]
+P_m2 = 10; % robot load mass [ kg ]
+P_l1 = 0.8; % upper arm length [ m ];
+P_l2 = 0.7; % lower arm length [ m ];
+P_g = 9.81;% gravity [ m/s^2 ]
