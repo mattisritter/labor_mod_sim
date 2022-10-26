1354a5fd09fc5a92841236679e77363c30641b0c
Marc Grosse <mgrosse.uni@gmail.com>
2022-10-26 12:04:54 +0200

    changed _data file

--- src/robot_data.m
+++ src/robot_data.m
@@ -4,3 +4,10 @@ P_m2 = 10; % robot load mass [ kg ]
 P_l1 = 0.8; % upper arm length [ m ];
 P_l2 = 0.7; % lower arm length [ m ];
 P_g = 9.81;% gravity [ m/s^2 ]
+
+%% Statevector x
+x_0 = [pi, pi/2, 0, 0];
+
+%% Massmatrix M
+
+M = [(P_m1+P_m2)*P_l1^2, P_m2*P_l1*P_l2*cos];
