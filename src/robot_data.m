b2a6c8e390f25beeb2bc2598b65bcdfdc8c225b0
Marc Grosse <mgrosse.uni@gmail.com>
2022-11-22 16:30:34 +0100

    Simulink Update

--- src/robot_data.m
+++ src/robot_data.m
@@ -5,5 +5,5 @@ P_l1 = 0.8; % upper arm length [ m ];
 P_l2 = 0.7; % lower arm length [ m ];
 P_g = 9.81;% gravity [ m/s^2 ]
 
-%% Statevector x
-x_0 = [pi, pi/2, 0, 0];
+%% Initialstate x_0
+x_0 = [pi; pi/2; 0; 0];
